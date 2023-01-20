bb3c69eca75de62a0baf79b3d9e44f492b0751b0
Rahul <patothanos6@gmail.com>
2023-01-20 22:01:52 +0530

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../assets/google.png";
+import atlassian from "../../assets/Bitmap.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
+
+export { google, atlassian, dropbox, shopify, slack };
